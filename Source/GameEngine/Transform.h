0e61e50c64fe2947f87cc38186f38e5931e3c5de
jpmmaia <jpmmaia@gmail.com>
2018-07-04 19:42:37 +0300

    Initializing project.

---
+++ Source/GameEngine/Transform.h
@@ -0,0 +1,25 @@
+#ifndef MAIA_GAMEENGINE_TRANSFORM_H_INCLUDED
+#define MAIA_GAMEENGINE_TRANSFORM_H_INCLUDED
+
+#include <vector>
+
+namespace Maia::GameEngine
+{
+	class Transform
+	{
+	public:
+
+
+
+	private:
+
+		struct Data
+		{
+		};
+
+		static Data s_data;
+
+	};
+}
+
+#endif
