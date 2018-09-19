6f01863bb4206e47a5b9b6ba98f7fd4cc64ae79f
JPMMaia <jpmmaia@gmail.com>
2018-09-19 21:46:44 +0300

    Component group mask is sum of component's ID hash values.

--- Source/Maia/GameEngine/Component_group_builder.hpp
+++ Source/Maia/GameEngine/Component_group_builder.hpp
@@ -1,6 +1,9 @@
 #ifndef MAIA_GAMEENGINE_COMPONENTGROUPBUILDER_H_INCLUDED
 #define MAIA_GAMEENGINE_COMPONENTGROUPBUILDER_H_INCLUDED
 
+#include <array>
+#include <cstdint>
+
 #include <Maia/GameEngine/Component_group.hpp>
 
 namespace Maia::GameEngine
@@ -19,15 +22,17 @@ namespace Maia::GameEngine
 		template <class ComponentType>
 		void add_component()
 		{
-			// TODO calculate size of component
-			// TODO get component type ID and hash to build a Component_group_mask
+			m_component_group_hash += std::hash(ComponentType::ID());
+			m_components_sizes[m_component_count++] = sizeof(ComponentType);
 		}
 
 		Component_group build() const;
 
 	private:
 
-		
+		std::uint16_t m_component_group_hash;
+		std::array<std::uint16_t, 16> m_components_sizes;
+		std::size_t m_component_count;
 
 	};
 }
