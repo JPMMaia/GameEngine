267c9eaf17a7bd5558789436871a83ba65676e2d
JPMMaia <jpmmaia@gmail.com>
2018-10-24 21:08:02 +0300

    Create more unit tests for Entity_manager.

--- Test/UnitTest/Entity_manager.test.cpp
+++ Test/UnitTest/Entity_manager.test.cpp
@@ -32,7 +32,7 @@ namespace Maia::GameEngine::Test
 		}
 	}
 
-	/*SCENARIO("Create an entity constituted by a position and then destroy it")
+	SCENARIO("Create an entity constituted by a position and then destroy it")
 	{
 		GIVEN("An entity manager")
 		{
@@ -40,15 +40,15 @@ namespace Maia::GameEngine::Test
 
 			AND_GIVEN("A position")
 			{
-				const Position position{ -1.0f, 1.0f, 0.5f };
+				Position const position{ -1.0f, 1.0f, 0.5f };
 
 				WHEN("The entity type is created")
 				{
-					const auto entity_type = entity_manager.create_entity_type<Position>();
+					Entity_type const entity_type = entity_manager.create_entity_type<Position>();
 
 					AND_WHEN("The entity is created")
 					{
-						const auto entity = entity_manager.create_entity(entity_type);
+						Entity const entity = entity_manager.create_entity(entity_type);
 
 						AND_WHEN("The entity's position data is set")
 						{
@@ -56,11 +56,11 @@ namespace Maia::GameEngine::Test
 
 							AND_WHEN("The entity's position data is retrieved")
 							{
-								const auto queriedPosition = entity_manager.get_component_data<Position>(entity);
+								Position const queried_position = entity_manager.get_component_data<Position>(entity);
 
 								THEN("They must match")
 								{
-									CHECK(position == queriedPosition);
+									CHECK(position == queried_position);
 								}
 							}
 						}
@@ -83,5 +83,56 @@ namespace Maia::GameEngine::Test
 				
 			}
 		}
-	}*/
+	}
+
+	SCENARIO("Create an entity, then add a component type, set values, get values and finally remove it")
+	{
+		GIVEN("An entity manager")
+		{
+			Entity_manager entity_manager;
+
+			AND_GIVEN("An empty entity type")
+			{
+				Entity_type const entity_type = entity_manager.create_entity_type();
+
+				WHEN("An entity is created")
+				{
+					Entity const entity = entity_manager.create_entity(entity_type);
+
+					THEN("The entity manager should report that the entity hasn't any Position component associated with it")
+					{
+						CHECK(entity_manager.has_component<Position>() == false);
+					}
+
+					AND_WHEN("A component type is added to that entity")
+					{
+						Position const original_position { 1.0f, 2.0f, 3.0f };
+						entity_manager.add_component<Position>(entity, original_position);
+
+						THEN("The entity manager should report that the entity has a Position component associated with it")
+						{
+							CHECK(entity_manager.has_component<Position>());
+						}
+
+						THEN("The component data associated with the entity should have correspond to the given values")
+						{
+							Position const current_position = entity_manager.get_component_data<Position>(entity);
+
+							CHECK(original_position == current_position);
+						}
+
+						AND_WHEN("The position component is removed")
+						{
+							entity_manager.remove_component<Position>(entity);
+
+							THEN("The entity manager should report that the entity hasn't any Position component associated with it")
+							{
+								CHECK(entity_manager.has_component<Position>() == false);
+							}
+						}
+					}
+				}
+			}
+		}
+	}
 }
